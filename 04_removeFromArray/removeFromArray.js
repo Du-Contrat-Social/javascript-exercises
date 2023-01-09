6eda8af5c7dd6438dff5462322b4cd380a2e2276
Du-Contrat-Social <gotter.dammerung@outlook.com>
2023-01-09 16:03:22 -0500

    Completed removeFromArray function writing exercise in JS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+
+    let args = Array.from(arguments).slice(1);
+
+    for (let i = 0; i < args.length; i++) {
+      while (arr.includes(args[i])) {
+        arr.splice(arr.indexOf(args[i]), 1);
+      }
+    }
+
+    return arr;
 
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
+
