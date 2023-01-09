c0a74e488a9f6284d670ada1c7d1121b7a0b9a8d
Du-Contrat-Social <gotter.dammerung@outlook.com>
2023-01-09 00:24:57 -0500

    Completed repeatString function writing exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let finalString = '';
+
+    if (num < 0) {
+
+        return 'ERROR'
+
+    };
+
+
+    for (let i = 0; i < num; i++) {
+        
+        finalString = finalString + string;
+
+    };
+
+    return finalString;
 
 };
+
+let string = 'hey';
+let num = 3;
 
 // Do not edit below this line
 module.exports = repeatString;
