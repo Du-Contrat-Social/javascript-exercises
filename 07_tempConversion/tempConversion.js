9e00097376fdace4f6a07c20221539f51cb289a2
Du-Contrat-Social <gotter.dammerung@outlook.com>
2023-01-10 01:52:30 -0500

    Completed tempConversion function writing exercise in JS

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenTemp) {
+  let celsiusTemp = (fahrenTemp-32)*5/9;
+  return parseFloat(celsiusTemp.toFixed(1));
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+
+  let fahrenTemp = 9/5*celsiusTemp+32;
+  return parseFloat(fahrenTemp.toFixed(1));
+
 };
 
 // Do not edit below this line
